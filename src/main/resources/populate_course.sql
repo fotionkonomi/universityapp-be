116edf558c659f364abffc9634e3ce6e914cf72b
Fotion.Konomi@fti.edu.al <Fotion.Konomi@fti.edu.al>
2021-06-10 22:05:15 +0200

    #UA-11 populate course and hall tables in the database

---
+++ src/main/resources/populate_course.sql
@@ -0,0 +1,16 @@
+INSERT INTO course (id, created_at, updated_at, deleted, name, description, hall_id, start_date_time, repeat_count, repeat_type) VALUES
+	(1, NOW(), NOW(), 0, 'Inxhinieri Software 2', 'Lende qe na meson fazat dhe si te zhvillojme sisteme software ne menyren me te mire', 1, NOW(), 12, 1),
+	(2, NOW(), NOW(), 0, 'Cloud Computing', 'Lenda qe na meson rreth sistemeve cloud', 1, NOW(), 12, 1),
+	(3, NOW(), NOW(), 0, 'Kompilatoret', 'Lenda qe na meson rreth menyres se funksionimit te kompilatoreve', 1, NOW(), 12, 1),
+	(4, NOW(), NOW(), 0, 'Teori gjuhesh', 'Lenda qe na meson rreth teorise se gjuheve', 2, NOW(), 12, 1),
+	(5, NOW(), NOW(), 0, 'PNS', 'Lenda qe na meson rreth perpunimit numerik te sinjaleve ', 1, NOW(), 12, 1),
+	(6, NOW(), NOW(), 0, 'Programim i orientuar nga objekti', 'Lenda qe na meson rreth programimit te orientuar nga objekti, me konkretisht rreth gjuhes Java', 1, NOW(), 12, 1),
+	(7, NOW(), NOW(), 0, 'Arkitektura e kompjuterave', 'Lenda qe na meson rreth menyres sesi cdo instruksion ekzekutohet ne procesor', 1, NOW(), 12, 1),
+	(8, NOW(), NOW(), 0, 'Siguria e sistemeve', 'Lenda qe na meson koncepte se si te mbrohemi apo sulmojme sisteme te ndryshme kompjuterike', 1, NOW(), 12, 1),
+	(9, NOW(), NOW(), 0, 'Algoritem dhe programim i avancuar', 'Lenda qe na meson rreth strukturave te te dhenave dhe optimizimit te algoritmeve', 1, NOW(), 12, 1),
+	(10, NOW(), NOW(), 0, 'Fizika', 'Self explanatory', 1, NOW(), 12, 1),
+	(11, NOW(), NOW(), 0, 'Rrjetat e kompjuterave', 'Lenda qe na meson rreth rrjetave te kompjuterave', 1, NOW(), 12, 1),
+	(12, NOW(), NOW(), 0, 'Menaxhimi i projekteve', 'Lenda qe na meson rreth menyres se si behet menaxhimi i projekteve', 1, NOW(), 12, 1),
+	(13, NOW(), NOW(), 0, 'Sisteme operative', 'Lenda qe na meson rreth menyres se funksionimit te sistemeve operative', 1, NOW(), 12, 1),
+	(14, NOW(), NOW(), 0, 'Kerkime operacionale', 'Lenda qe na meson rreth menyrave te optimizimit matematik', 1, NOW(), 12, 1);
+
---
+++ src/main/resources/populate_hall.sql
@@ -0,0 +1,3 @@
+INSERT INTO hall (id, created_at, updated_at, deleted, name, description, latitude, longitude) VALUES
+	(1, NOW(), NOW(), 0, 'Silvano Pedrollo', 'Salle ne UPT', 41.3170883238892, 19.821528642329216),
+	(2, NOW(), NOW(), 0, 'Klasa 100', 'Salle ne FSHN', 41.33461980607517, 19.816429040249176);
